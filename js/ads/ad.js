8fc65432f4ef8c71a8bdedbcf4edcd37e5a728c6
DwaynetheHero09 <windowswallpapersarchive@gmail.com>
2024-06-30 18:36:02 -0400

    Update ad.js

--- js/ads/ad.js
+++ js/ads/ad.js
@@ -1,15 +1,3 @@
-var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
-if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
-{
-  let window = _____WB$wombat$assign$function_____("window");
-  let self = _____WB$wombat$assign$function_____("self");
-  let document = _____WB$wombat$assign$function_____("document");
-  let location = _____WB$wombat$assign$function_____("location");
-  let top = _____WB$wombat$assign$function_____("top");
-  let parent = _____WB$wombat$assign$function_____("parent");
-  let frames = _____WB$wombat$assign$function_____("frames");
-  let opener = _____WB$wombat$assign$function_____("opener");
-
 if(typeof KIDS == "undefined" || !KIDS) var KIDS = {};
 
 KIDS.namespace("ads");
@@ -61,26 +49,3 @@ KIDS.ads.getVideoAdFrequency = function(){
 		else return 0;
 	}catch(e){return 0}	
 }
-
-
-}
-/*
-     FILE ARCHIVED ON 21:58:28 Jan 09, 2013 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 21:59:08 May 01, 2023.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
-/*
-playback timings (ms):
-  captures_list: 175.088
-  exclusion.robots: 0.136
-  exclusion.robots.policy: 0.127
-  cdx.remote: 0.069
-  esindex: 0.008
-  LoadShardBlock: 135.167 (3)
-  PetaboxLoader3.datanode: 136.997 (4)
-  load_resource: 60.391
-  PetaboxLoader3.resolve: 32.067
-*/
