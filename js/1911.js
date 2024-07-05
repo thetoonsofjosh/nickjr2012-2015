ce768cd68f48e730711773d34459b8201ad917a9
DwaynetheHero09 <windowswallpapersarchive@gmail.com>
2024-07-05 18:50:25 -0400

    Add files via upload

---
+++ js/1911.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+CE2 = undefined;
+/* Inactive account */
+
+}
+/*
+     FILE ARCHIVED ON 09:55:51 Jul 30, 2014 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 16:43:01 May 11, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.784
+  exclusion.robots: 0.268
+  exclusion.robots.policy: 0.255
+  esindex: 0.011
+  cdx.remote: 16.318
+  LoadShardBlock: 172.696 (3)
+  PetaboxLoader3.datanode: 1326.598 (5)
+  PetaboxLoader3.resolve: 171.43 (3)
+  load_resource: 1391.966 (2)
+*/
