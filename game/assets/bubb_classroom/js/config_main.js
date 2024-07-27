272a5f1690de8192a35641788379eb3f6809e94f
DwaynetheHero <166329275+DwaynetheHero@users.noreply.github.com>
2024-07-27 17:11:46 -0400

    Files upload

---
+++ game/assets/bubb_classroom/js/config_main.js
@@ -0,0 +1,75 @@
+{
+	"config":{
+		
+		"debug":{
+			"hotkeys":true,
+			"autoSkipIntro":false
+		},
+		
+		"copyright":"©2010 Viacom International Inc. All Rights Reserved. Nickelodeon, Bubble Guppies and all related titles, logos and characters are trademarks of Viacom International Inc.",
+		
+		"files":{
+			"mp3":[
+				// drop anim vo
+				"ambulance",
+				"barn",
+				"blocks_main",
+				"books_main",
+				"can_plant",
+				"chest",
+				"easel_crayons",
+				"fire_engine",
+				"prop_airplane",
+				"prop_aquarium",
+				"prop_ball",
+				"prop_boombox",
+				"prop_cashregister",
+				"recycle",
+				
+				// button rollover vo
+				"back",
+				"next",
+				"play",
+				"play_again",
+				"print",
+				"skip",
+				"tab_bubbleguppies",
+				"tab_classroom_stuff",
+				"trash",
+				"yes",
+				"no"
+			]
+		},
+		
+		"menu":{
+			"buttonOrder":{
+				"characters":[
+					"nonny",
+					"gil",
+					"goby",
+					"deema",
+					"oona",
+					"molly",
+					"mrgrouper",
+					"bubblepuppy"
+				],
+				"items":[
+					"ambulance",
+					"barn",
+					"blocks_main",
+					"books_main",
+					"can_plant",
+					"chest",
+					"easel_crayons",
+					"fire_engine",
+					"prop_airplane",
+					"prop_aquarium",
+					"prop_ball",
+					"prop_boombox",
+					"prop_cashregister",
+					"recycle"
+				]
+			}
+		}
+	}
+}
